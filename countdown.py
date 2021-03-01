fa2f2bd3aa26d792c73148a8e7f81144275a8011
Kevin C. Escobedo <escobedo001@gmail.com>
2021-03-01 15:02:56 -0800

    Calculates the days between now and a specified day

---
+++ countdown.py
@@ -0,0 +1,25 @@
+import datetime
+
+def getNextYear(now: datetime.datetime, date: datetime.datetime) -> int:
+    '''Returns the next year if this year's date has passed'''
+    if now.month >= date.month and now.day >= date.day:
+        return now.year + 1
+    else:
+        return now.year
+
+#If the day is the specified date, return the next year
+assert(getNextYear(datetime.datetime(2020, 12, 25, 12, 0, 0), datetime.datetime(2020, 12, 25, 12, 0, 0)) == 2021)
+#Specified date year does not matter, only month and day
+assert(getNextYear(datetime.datetime(2020, 12, 26, 12, 0, 0), datetime.datetime(2000, 12, 25, 12, 0, 0)) == 2021)
+#If the day is before the specified date, return same year 
+assert(getNextYear(datetime.datetime(2020, 1, 1, 12, 0, 0), datetime.datetime(2021, 12, 25, 12, 0, 0)) == 2020)
+#If the day is after the specified date, return next year
+assert(getNextYear(datetime.datetime(2020, 12, 30, 12, 0, 0), datetime.datetime(2020, 12, 25, 12, 0, 0)) == 2021)
+
+def getDaysUntil(date: datetime.datetime) -> int:
+    '''Calculates the days until the given date'''
+    now = datetime.datetime.now()
+    year = getNextYear(now, date)
+    date = datetime.datetime(year, date.month, date.day)
+    delta = date - now
+    return delta.days
